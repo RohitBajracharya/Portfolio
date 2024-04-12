f49e86a15cced493508e7370ce2dcf7e3cb46c86
RohitBajra <104704874+rohitnewgit@users.noreply.github.com>
2024-04-12 17:14:37 +0545

    sidebar updated

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -30,7 +30,7 @@ const Sidebar = () => {
       <motion.div className="bg" variants={variants}>
         <Links />
       </motion.div>
-      <ToggleButton setOpen={setOpen} />
+      <ToggleButton open={open} setOpen={setOpen} />
     </motion.div>
   );
 };
--- src/components/sidebar/toggleButton/ToggleButton.jsx
+++ src/components/sidebar/toggleButton/ToggleButton.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 const ToggleButton = ({ setOpen }) => {
   return (
     <button onClick={() => setOpen((prev) => !prev)}>
-      <svg width="23" height="23" viewBox="0 0 23 23">
+      <svg width="20" height="20" viewBox="0 0 20 20">
         <motion.path
           strokeWidth="3"
           stroke="black"
